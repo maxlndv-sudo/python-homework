9318df34e7bb737b62ec99850c6c264424af7458
maxlndv-sudo <maxlndv@gmail.com>
2026-06-19 16:40:32 +0800

    Update ex04.py

--- chapter16/ex04.py
+++ chapter16/ex04.py
@@ -1,1 +1,14 @@
+def mul_time(time, number):
+    """Умножает время на число (например, для расчета интервалов)."""
+    # Используем функции преобразования из Упражнения 3
+    seconds = time_to_int(time) * number
+    return int_to_time(int(seconds))
+
+def find_average_pace(finish_time, distance):
+    """Вычисляет средний темп (время на одну милю/километр).
+    finish_time: объект Time (общее время гонки)
+    distance: число (дистанция)
+    """
+    # Разделить время на дистанцию — это то же самое, что умножить на (1 / дистанция)
+    return mul_time(finish_time, 1 / distance)
 
